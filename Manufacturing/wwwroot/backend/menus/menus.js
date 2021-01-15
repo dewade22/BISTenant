5cabb5e5a6afb8cded1de9383ea57f14e53bbdde
dewade22 <dewadwizx@gmail.com>
2021-01-15 12:19:29 +0800

    Menambah JS untuk menu Baru

---
+++ Manufacturing/wwwroot/backend/menus/menus.js
@@ -0,0 +1,24 @@
+﻿let tenantAct = $("#tenantLayout").val().trim()
+let baseUrls = window.location.origin
+localStorage.setItem("thisAddress", baseUrls + '/' + tenantAct)
+    $(function () {
+        $.ajax({
+            type: "POST",
+            url: baseUrls + "/" + tenantAct + "/Dashboard/GetDashboardsList",
+            error: function (result) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: result
+                })
+            },
+            success: function (result) {
+                let menu="";
+                $.each(result, function (key, value) {
+                    menu += '<li><a href="' + baseUrls + '/' + tenantAct + '/Dashboard/Dashboard/' + value.id + '">' + value.name + '</a></li>';                  
+                })
+                $("#menuDash").html(menu)
+            }
+
+        })
+    })
