1baa2b053e608980c91243119d84cb356d51ff77
Dewa Dwi <51353195+dewade22@users.noreply.github.com>
2021-03-18 23:02:49 +0800

    Valuation alomst done

--- Manufacturing/wwwroot/backend/Inventory/ValuationForm.js
+++ Manufacturing/wwwroot/backend/Inventory/ValuationForm.js
@@ -1,6 +1,47 @@
-﻿
-let baseUrl = localStorage.getItem('thisAddress');
+﻿let baseUrl = localStorage.getItem('thisAddress');
+
+let dataTables = $('#tableValuation').DataTable({
+    "scrollX": true,
+    "autoWidth": true,
+    "columnDefs": [
+        {
+            "render": function (data, type, row) {
+                return data.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+            },
+            "targets": [3,4,5,7,8,9,11,12,13,15,16,17]
+        },
+        {
+            "render": function (data, type, row) {
+                let result = row[5] == 0 ? 0 : row[3] == 0 ? 0 : row[5]/row[3]
+                return result.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+            },
+            "targets": 6
+        },
+        {
+            "render": function (data, type, row) {
+                let result = row[9] == 0 ? 0 : row[7] == 0 ? 0 : row[9] / row[7]
+                return result.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+            },
+            "targets": 10
+        },
+        {
+            "render": function (data, type, row) {
+                let result = row[13] == 0 ? 0 : row[11] == 0 ? 0 : row[13] / row[11]
+                return result.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+            },
+            "targets": 14
+        },
+        {
+            "render": function (data, type, row) {
+                let result = row[17] == 0 ? 0 : row[15] == 0 ? 0 : row[17] / row[15]
+                return result.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+            },
+            "targets": 18
+        }
+    ]
+});
 $('#search').click(function () {
+    jQuery('#tableValuation tbody').empty();
     doSomething($('#startDate').val(), $('#endDate').val(), $('#invenRpt').val(), $('#cboCategory').val(), $('#cboProductGroups').val(), $('#cboLocation').val(), $('#cboFlavour').val(), $('#cboSize').val())
 })
 
@@ -35,6 +76,7 @@ function doSomething(startingDate = '', endingDate = '', invenRPT = '', category
             })
         }
         else {
+            dataTables.clear();
             //Ajax
             $.ajax({
                 type: 'GET',
@@ -52,8 +94,29 @@ function doSomething(startingDate = '', endingDate = '', invenRPT = '', category
                 contentType: 'application/json',
                 success: function (result) {
                     result.forEach(function (result) {
-                        console.log(result.itemNo)
+                        dataTables.row.add([
+                            result.inventoryPostingGroup,
+                            result.itemCategoryCode,
+                            result.description,
+                            result.qtyStarting,
+                            result.literStarting,
+                            result.valueStarting,
+                            0,
+                            result.qtyIn,
+                            result.literIn,
+                            result.valueIn,
+                            0,
+                            result.qtyOut,
+                            result.literOut,
+                            result.valueOut,
+                            0,
+                            result.qtyEnding,
+                            result.literEnding,
+                            result.valueEnding,
+                            0
+                        ])
                     })
+                    dataTables.draw();
                 }
             })
         }
